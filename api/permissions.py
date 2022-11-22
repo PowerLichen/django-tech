0b91f7e03b9fd85c8de2fa65163034f318671cd2
PowerLichen <ms.choe015@gmail.com>
2022-11-23 00:17:29 +0900

    feat: add permission base code

--- api/permissions.py
+++ api/permissions.py
@@ -1,8 +1,25 @@
+import requests
 from rest_framework import permissions
+from rest_framework.exceptions import NotFound
 
 class ExternalAuthPermission(permissions.BasePermission):
+    # called on all HTTP requests
     def has_permission(self, request, view):
-        return super().has_permission(request, view)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # use external auth check
+        token = request.META.get('HTTP_AUTHORIZATION')
+        url = 'http://spring-server:8080/api/v1/accounts/token'
+        try:
+            res = requests.get(url, headers={'Authorization': token})
+        except requests.exceptions.ConnectionError as err:
+            print("ConnectionError:", err)
+            raise NotFound
+
+        if res.status_code != requests.codes.ok:
+            return False        
+        return True
     
     def has_object_permission(self, request, view, obj):
         return super().has_object_permission(request, view, obj)

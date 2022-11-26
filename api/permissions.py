cb3e669ca0a40330feb62b43c9e10cbe7437069b
PowerLichen <ms.choe015@gmail.com>
2022-11-26 19:58:51 +0900

    fix: permission url change

--- api/permissions.py
+++ api/permissions.py
@@ -5,9 +5,9 @@ from rest_framework.exceptions import NotFound
 class ExternalAuthPermission(permissions.BasePermission):
     # called on all HTTP requests
     def has_permission(self, request, view):        
-        # use external auth check
+        # use external auth check by jwt
         token = request.META.get('HTTP_AUTHORIZATION')
-        url = 'http://spring-server:8080/api/v1/accounts/token'
+        url = 'http://external-auth-server:8000/token'
         try:
             res = requests.get(url, headers={'Authorization': token})
         except requests.exceptions.ConnectionError as err:

9ff60d5d6be1c3e07d5138a9d199ed04356efa83
PowerLichen <ms.choe015@gmail.com>
2023-03-20 22:07:12 +0900

    feat: add extra action code

--- api/extraaction/serializers.py
+++ api/extraaction/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 
 from api.extraaction.models import ExtraModel
 
+
 class ExtraModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExtraModel
-        fields = ['id', 'title', 'desc', 'password' ]
+        fields = ["id", "title", "desc", "password" ]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+class ExtraModelPasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtraModel
+        fields = ["password"]
--- api/extraaction/views.py
+++ api/extraaction/views.py
@@ -3,17 +3,22 @@ from rest_framework.decorators import action
 
 from api.extraaction.models import ExtraModel
 from api.extraaction.serializers import ExtraModelSerializer
+from api.extraaction.serializers import ExtraModelPasswordSerializer
 
 
 class ExtraActionViewSet(ModelViewSet):
     queryset = ExtraModel.objects.all()
     serializer_class = ExtraModelSerializer
 
+    def get_serializer_class(self):
+        if self.action == "set_password":
+            return ExtraModelPasswordSerializer
+        return super().get_serializer_class()
 
     @action(detail=False, methods=['get'], url_path='last')
     def get_last(self, request, *args, **kwargs):
-        pass
+        return super().list(request, *args, **kwargs)
 
     @action(detail=True, methods=['post'], url_path="password")
     def set_password(self, request, *args, **kwargs):
-        pass
+        return super().partial_update(request, *args, **kwargs)
